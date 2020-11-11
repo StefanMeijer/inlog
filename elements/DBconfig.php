1477afb3737053e666b8ae1b004d5a154b5af283
Stefan <shh.meijer@student.alfa-college.nl>
2020-11-11 02:35:52 +0100

    Corrected dbconfig

--- elements/DBconfig.php
+++ elements/DBconfig.php
@@ -3,7 +3,7 @@
 DEFINE("DB_USER", "root"); // username database
 DEFINE("DB_PASS", ""); // password database
 try {
-    $db = new PDO("mysql:host=localhost;dbname=project_veilig", DB_USER, DB_PASS); //host & database name
+    $db = new PDO("mysql:host=localhost;dbname=inlog", DB_USER, DB_PASS); //host & database name
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage();
