99b7752fa566c41367b3a0ab27559be819262f8c
Stefan <shh.meijer@student.alfa-college.nl>
2020-11-11 02:35:20 +0100

    Made index correctly

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ session_start();
 require_once("elements/elements.php");
 require_once("elements/DBconfig.php");
 
-//  Set homepage to 'home'.php
+//  Set homepage to 'home'.php in page/
 $page = 'home';
-
-if (isset($_GET["page"])) {
+if (isset($_GET["page"]) && !empty($_GET['page'])) {
     $page = $_GET["page"];
 }
 
-include(sprintf('%s/%s.php', 'page', $page));
-// include(sprintf('%1$s/%2$s.php', 'content', $content)); dit kan ook, het is maar net wat je wil
-// je kunt in de header nu switchen van content door bijv. localhost/index.php?pagw=andereContent in te vulen
+//Include of the page's
+include_once('elements/header.php');
+include_once(sprintf('%s/%s.php', 'page', $page));
+include_once('elements/footer.php');
